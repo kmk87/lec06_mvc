1d8d04244ebd685d0e45ad6f3d315439f9c2c409
kmk87 <minkyoungk24@gmail.com>
2024-07-09 15:02:49 +0900

    [김민경] 비밀번호 암호화

--- src/main/java/com/gn/user/controller/UserCreateEndServlet.java
+++ src/main/java/com/gn/user/controller/UserCreateEndServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/user/createEnd")
+@WebServlet(name="userCreateEnd",urlPatterns="/user/createEnd")
 public class UserCreateEndServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
